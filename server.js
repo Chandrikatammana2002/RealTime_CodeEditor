43525e2cf334c96865447ca366d97d5c412467b6
Chandrikatammana2002 <95030840+Chandrikatammana2002@users.noreply.github.com>
2023-09-12 18:19:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,14 +22,14 @@ const io=new Server(server,{           //in server class we need to pass http se
 
 })
 // We are storing mapping here in memory, by restarting server everything will be lost, if production level app we have to store in db,file etc
-
+/*
 // Whenever we get request to build then automatically it calls index.js in build/static it is built and displayed in browser
 app.use(express.static("build"));
 // We are saying to the serverv that whatever request we get server them index.html
 app.use((req, res, next) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
-
+*/
 
 
 app.use(cors());
@@ -93,5 +93,5 @@ app.get("/",(req,res)=>{
   
 
 // listening on 5000 port
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
